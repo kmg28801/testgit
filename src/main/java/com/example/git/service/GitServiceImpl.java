36e60efe3c804e60e27c247dacb20a273e19ad95
kmg28801 <kmg08801@gmail.com>
2022-07-20 22:08:04 +0900

    feature2 2nd commit

--- src/main/java/com/example/git/service/GitServiceImpl.java
+++ src/main/java/com/example/git/service/GitServiceImpl.java
@@ -3,6 +3,6 @@ package com.example.git.service;
 public class GitServiceImpl implements GitService {
     @Override
     public void join() {
-        
+        System.out.println("true = " + true);
     }
 }
