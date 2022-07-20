983c641170880da544ae1d524e873583a1a8ca9a
kmg28801 <kmg08801@gmail.com>
2022-07-20 22:23:32 +0900

    feature1 2nd commit

--- src/main/java/com/example/git/feature1/Feature1.java
+++ src/main/java/com/example/git/feature1/Feature1.java
@@ -4,6 +4,11 @@ public class Feature1 {
 
     void test() {
         System.out.println("Feature1.test");
+
+    }
+
+    void test2() {
+        System.out.println("Feature1.test2");
         
     }
 }
